b314cf41ea7345ae2b2ca3c7f2df74ae15463b88
wdj1 <wdj1@foxmail.com>
2018-07-02 14:38:13 +0800

    删除任务

--- js/main.js
+++ js/main.js
@@ -20,8 +20,12 @@
       update: function() {
 
       },
-      remove: function() {
-
+      remove: function(id) {
+        // splice(index, howmany, items)
+        // index     必需。整数，规定添加/删除项目的位置，使用负数可从数组结尾处规定位置。
+        // howmany   必需。要删除的项目数量。如果设置为 0，则不会删除项目。
+        // items     可选。向数组添加的新项目。
+        this.list.splice(id, 1);
       }
     }
   })
