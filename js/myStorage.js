2711e09d159c464931c415c7e9e83b0b4aaba1b4
wdj1 <wdj1@foxmail.com>
2018-07-02 13:10:59 +0800

    localStorage的封装

---
+++ js/myStorage.js
@@ -0,0 +1,24 @@
+/* myStorage.js */
+;(function () {
+  window.ms = {
+    set: set,
+    get: get,
+  };
+
+  function set(key, val) {
+    // 用JSON把数组和对象转换成字符串
+    localStorage.setItem(key, JSON.stringify(val));
+  }
+
+  function get(key) {
+    var json = localStorage.getItem(key);
+    if (json) {
+      return JSON.parse(json);
+    }
+  }
+})();
+
+
+ms.set('name',  '王花花');
+var name = ms.get('name');
+console.log('name:', name);
